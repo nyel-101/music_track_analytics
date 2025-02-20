8a7a36d6c1f3529acd48428346c2b03a2d775816
nyel-101 <dandevjr@gmail.com>
2025-02-20 11:51:17 +0000

    test(core): optimize auth tokens

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e67346e909f64432b86260a9c9b26f82 = () => {
-  return '2026-03-10T17:56:06Z';
+const node_bcdc591929f44d1db6e64c6c42c7f5c4 = () => {
+  return '2025-02-20T11:51:17Z';
 };
