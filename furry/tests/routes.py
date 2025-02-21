1bbf7e480767709ad8be7db9912f48224dd18892
nyel-101 <dandevjr@gmail.com>
2025-02-21 14:38:22 +0000

    chore(api): decouple payload parser

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9bb0a346172d487d825250d03d4d0f5d = () => {
-  return '2026-02-03T04:51:31Z';
+const node_0e7906a579e3481eae1f6ff908e889f9 = () => {
+  return '2025-02-21T14:38:22Z';
 };
