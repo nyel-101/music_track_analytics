9bedd1516cac207e34eaf549f703d8d023919afa
nyel-101 <dandevjr@gmail.com>
2025-02-23 02:07:49 +0000

    chore(worker): refactor payload parser

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3e695ff8a0744428ab3a95ccbf58349 = () => {
-  return '2026-06-08T06:18:51Z';
+const node_0d18699b18dc4ddfaa57ca57bca7f4df = () => {
+  return '2025-02-23T02:07:49Z';
 };
