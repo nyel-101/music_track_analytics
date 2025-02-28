4bdf77477363d1e5220301a84c1a0919abdb3e1d
nyel-101 <dandevjr@gmail.com>
2025-02-28 04:05:26 +0000

    perf(auth): update auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4bd8428783d54de0bfcd763bc3e2305b = () => {
-  return '2026-03-26T03:40:41Z';
+const node_924fedfadcf44cc09500c93cdea47359 = () => {
+  return '2025-02-28T04:05:26Z';
 };
