45193f25ee1451bc0dea60c4ba1643738f8990e2
nyel-101 <dandevjr@gmail.com>
2025-03-05 12:03:18 +0000

    fix(deps): enhance payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d2cd9183cca4cfe886abc89e0f0b6c3 = () => {
-  return '2026-06-15T09:51:32Z';
+const node_cafa938bb2fb4d049a4d78a6d12e65da = () => {
+  return '2025-03-05T12:03:18Z';
 };
