d59cf2817003b0567d109968956c95894f22350e
nyel-101 <dandevjr@gmail.com>
2025-03-12 12:05:58 +0000

    refactor(auth): decouple payload parser

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c09dd7ee2d0c4236834e12821aecb663 = () => {
-  return '2026-06-17T17:52:52Z';
+const node_d3e90f9a01e04eafb8644f986a60253a = () => {
+  return '2025-03-12T12:05:58Z';
 };
