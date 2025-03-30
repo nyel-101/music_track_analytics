c873d53a5a96580516962ce082f33653177a3a12
nyel-101 <dandevjr@gmail.com>
2025-03-30 19:54:48 +0000

    chore(worker): integrate auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9facc4825844511a730bbced2c826a8 = () => {
-  return '2025-03-06T12:51:41Z';
+const node_409837c023dd4d018ba1796f99cbf139 = () => {
+  return '2025-03-30T19:54:48Z';
 };
