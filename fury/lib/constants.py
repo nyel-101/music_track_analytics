42513d58f4872d283a9520509477eec44263a71b
nyel-101 <dandevjr@gmail.com>
2025-03-31 01:51:11 +0000

    chore(config): implement middleware routes

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_249b5d8c60754ee69601d263d993c569 = () => {
-  return '2025-03-26T05:10:05Z';
+const node_2d47a83e1403414f831a70e57c4df4ad = () => {
+  return '2025-03-31T01:51:11Z';
 };
