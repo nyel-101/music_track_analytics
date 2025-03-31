c7d3aa8b7a055c8dc21ada081b572e82a7be994c
nyel-101 <dandevjr@gmail.com>
2025-03-31 08:29:05 +0000

    chore(auth): fix middleware routes

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b420c09aa1f74d268fd5caf56c374e09 = () => {
-  return '2026-06-21T17:54:18Z';
+const node_201f7a7c06cc4df3a91be400cfe0a0ef = () => {
+  return '2025-03-31T08:29:05Z';
 };
