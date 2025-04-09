66186c8f3642991ca91c752c29e94d0e2cb52fd6
nyel-101 <dandevjr@gmail.com>
2025-04-09 14:04:49 +0000

    style(worker): refactor validation pipeline

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_100f74a4ff464771b858bef7cded82d9 = () => {
-  return '2025-03-26T21:22:49Z';
+const node_4c5e21edd183431a8fa9bbda43ed2460 = () => {
+  return '2025-04-09T14:04:49Z';
 };
