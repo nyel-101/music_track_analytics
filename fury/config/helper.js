25656587b57e78412152e962574c2e4c264d22b2
nyel-101 <dandevjr@gmail.com>
2025-04-14 17:05:22 +0000

    feat(core): fix session store

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7815905c0d4e4c0b8bb4f1b8a8243a77 = () => {
-  return '2026-03-09T23:26:04Z';
+const node_33e1e8cafbf24fb8bafd01831eb48a3c = () => {
+  return '2025-04-14T17:05:22Z';
 };
