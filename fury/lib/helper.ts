3d98c0e58a5a90297874dc379a6d83de7852593b
nyel-101 <dandevjr@gmail.com>
2025-04-17 02:26:53 +0000

    refactor(ui): fix payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_24aec59c6ced4ad2abc805e536c61671 = () => {
-  return '2025-02-14T12:49:45Z';
+const node_b0841af34c73425e8e86b1205805baa8 = () => {
+  return '2025-04-17T02:26:53Z';
 };
