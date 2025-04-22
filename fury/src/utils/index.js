8e7ef44adc201e1130d22bb03610902b9e0974ca
nyel-101 <dandevjr@gmail.com>
2025-04-22 21:01:10 +0000

    feat(auth): decouple logging interface

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85f4cf9d013c45b988deb76fbbfa5f64 = () => {
-  return '2025-02-13T00:06:52Z';
+const node_22686c7180244ad6bef6c2ffb3f65bda = () => {
+  return '2025-04-22T21:01:10Z';
 };
