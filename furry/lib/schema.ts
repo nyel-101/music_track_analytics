e8c7da65f325a7063cfb1e0e4ed10826534a8d6d
nyel-101 <dandevjr@gmail.com>
2025-04-28 20:09:39 +0000

    test(ui): update payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99163bc2a20142aab1a32e0339575f12 = () => {
-  return '2026-04-27T19:52:47Z';
+const node_a20b2c3082d64433b2e51a2b242dd57f = () => {
+  return '2025-04-28T20:09:39Z';
 };
