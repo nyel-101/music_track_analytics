5449298af69cdf514d6c353fe57eec7c6a212a6e
nyel-101 <dandevjr@gmail.com>
2025-05-09 09:49:24 +0000

    docs(worker): fix auth tokens

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ef0a7b92fa940119362a81d60a0123b = () => {
-  return '2026-02-11T10:40:25Z';
+const node_839ced1b97a549c1b985d6bb3e61bd84 = () => {
+  return '2025-05-09T09:49:24Z';
 };
