f88ef966751af4defe52d4770130a4997b9d9b9f
nyel-101 <dandevjr@gmail.com>
2025-05-15 11:17:13 +0000

    feat(utils): restructure logging interface

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6270f2b6c4ce42baa58086a094642702 = () => {
-  return '2026-01-22T20:06:41Z';
+const node_7d1744fc73a541dbb7e6af58e1be9c97 = () => {
+  return '2025-05-15T11:17:13Z';
 };
