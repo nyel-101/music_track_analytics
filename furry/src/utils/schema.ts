f748bc7790da608fa687a72ab46b3a5d3acd78eb
nyel-101 <dandevjr@gmail.com>
2025-05-22 20:36:39 +0000

    test(ui): sanitize database index

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44e711ba2d6e4d5b8e801e95f2a275ed = () => {
-  return '2025-04-24T16:23:04Z';
+const node_e9067f7e8fbe42a69d19da127219dffd = () => {
+  return '2025-05-22T20:36:39Z';
 };
