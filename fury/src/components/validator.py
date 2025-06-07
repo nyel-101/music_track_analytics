041263b1f568ed48be4efe3aa52fc3c750dbc5bf
nyel-101 <dandevjr@gmail.com>
2025-06-07 20:54:29 +0000

    chore(auth): decouple view templates

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db2c113c09af42fe91dbc93ce14783cd = () => {
-  return '2025-04-09T08:58:55Z';
+const node_eccc1e8dff1b4fafb1dc94d155c86ddc = () => {
+  return '2025-06-07T20:54:29Z';
 };
