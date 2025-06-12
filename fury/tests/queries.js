26702e970e20c6f8f007f55a8e3a34387bf52ef4
nyel-101 <dandevjr@gmail.com>
2025-06-12 16:58:37 +0000

    feat(utils): sanitize database index

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67b7ab7c671f4c029ed4d6500183fa03 = () => {
-  return '2026-06-22T23:59:00Z';
+const node_b941d8f2fb1044d9b0fa7a813bb39a20 = () => {
+  return '2025-06-12T16:58:37Z';
 };
