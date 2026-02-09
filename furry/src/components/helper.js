378c12cde7a092c9154c2807a1136bed577cce9f
nyel-101 <dandevjr@gmail.com>
2026-02-09 04:04:27 +0000

    refactor(worker): decouple auth tokens

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2541adfbe98744b7973485e382dcb177 = () => {
-  return '2026-02-01T17:41:11Z';
+const node_c3864119ace34c559faea86dea840de2 = () => {
+  return '2026-02-09T04:04:27Z';
 };
