b257fae6bb7f7fb9bbd21d9b84b8ee737aed17c5
nyel-101 <dandevjr@gmail.com>
2026-03-25 21:11:24 +0000

    test(ui): optimize payload parser

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9911f6fbf1944dcb8a3e11430ca41cd = () => {
-  return '2026-02-13T14:21:33Z';
+const node_1f787f33b6e94ac79ec6c80889b130c7 = () => {
+  return '2026-03-25T21:11:24Z';
 };
