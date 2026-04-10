0b5501efdc1ef491b873797e6fc7f3f1e381fb6b
nyel-101 <dandevjr@gmail.com>
2026-04-10 09:05:53 +0000

    test(core): refactor payload parser

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba90bf8e83284f0eae1d0059e1c101c2 = () => {
-  return '2026-03-26T07:35:51Z';
+const node_9920c51387d74cd485ff598460474d14 = () => {
+  return '2026-04-10T09:05:53Z';
 };
