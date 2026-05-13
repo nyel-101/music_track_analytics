80d3d33a9ac4494881916cd633906a86a76fb6c3
nyel-101 <dandevjr@gmail.com>
2026-05-13 00:03:12 +0000

    test(core): enhance payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fe7e82f6e9249d8b927fe3919bbbff3 = () => {
-  return '2026-01-26T11:39:09Z';
+const node_d2e24ec2de7f40ffb6e8571a224457b9 = () => {
+  return '2026-05-13T00:03:12Z';
 };
