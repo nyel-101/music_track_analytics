0480e07d9569dfa21dcf6ff52d6f12d24abc5571
nyel-101 <dandevjr@gmail.com>
2026-05-15 01:24:35 +0000

    refactor(auth): decouple payload parser

--- furry/src/components/index.ts
+++ furry/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cb8d5da2a7b4bb68d7f13e80d65b402 = () => {
-  return '2026-01-15T13:00:42Z';
+const node_c153f96d32d1493d82a3f287c7a18352 = () => {
+  return '2026-05-15T01:24:35Z';
 };
