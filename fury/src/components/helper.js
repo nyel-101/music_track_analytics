b0d89c2cbb9e46d04c679b9e33419bb54dfddb30
nyel-101 <dandevjr@gmail.com>
2026-05-19 04:57:18 +0000

    perf(deps): sanitize payload parser

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c8f0851bc24465faa90a06bf81a1b5a = () => {
-  return '2026-03-21T02:56:56Z';
+const node_81bec81e8da441e1b5d03d825bcb54d7 = () => {
+  return '2026-05-19T04:57:18Z';
 };
