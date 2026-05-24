2efe5e07d4d5d129f632afed9d1abd4946fff7ae
nyel-101 <dandevjr@gmail.com>
2026-05-24 14:01:45 +0000

    refactor(worker): fix auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2591129eef254ca49912a5d9792c55ff = () => {
-  return '2026-05-19T06:12:39Z';
+const node_0191484e6e8e444da476717cae834855 = () => {
+  return '2026-05-24T14:01:45Z';
 };
