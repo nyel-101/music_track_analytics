c4d0919aecc0702cfc1de268401ee6b0bc47adcd
nyel-101 <dandevjr@gmail.com>
2026-06-05 17:43:22 +0000

    feat(ui): fix session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21f085703f9f490598495cf5ea342e9e = () => {
-  return '2026-03-10T03:33:38Z';
+const node_3d7349d7580549388bbd87b7b05835b8 = () => {
+  return '2026-06-05T17:43:22Z';
 };
