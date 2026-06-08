66d321d8b3ab6c3d3adca88a550282991498c12d
nyel-101 <dandevjr@gmail.com>
2026-06-08 12:21:15 +0000

    refactor(core): decouple logging interface

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d7349d7580549388bbd87b7b05835b8 = () => {
-  return '2026-06-05T17:43:22Z';
+const node_6490e1e1f133435696925a76ef8fd366 = () => {
+  return '2026-06-08T12:21:15Z';
 };
