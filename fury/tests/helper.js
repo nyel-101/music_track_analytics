a1f30da959df3b813013cbb4dca25c4bc1b3a6d5
nyel-101 <dandevjr@gmail.com>
2026-06-16 23:24:08 +0000

    style(config): optimize session store

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6666a60c50664da0ab715151fd2753f1 = () => {
-  return '2026-04-24T19:53:38Z';
+const node_c48db15f77684ac6a7ac074dea77dbb9 = () => {
+  return '2026-06-16T23:24:08Z';
 };
