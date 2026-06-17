5420464df071b52c2b6f6263bc591928215ea0d7
nyel-101 <dandevjr@gmail.com>
2026-06-17 23:58:31 +0000

    refactor(auth): enhance view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c1cdc6bc617146e6a261f605cfc7c997 = () => {
-  return '2026-06-08T04:22:40Z';
+const node_89375af67fe04632a4d9a33525b18cb5 = () => {
+  return '2026-06-17T23:58:31Z';
 };
