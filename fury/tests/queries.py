8bde4a648c1f53443e3eeb3a1ea53b08cca01ac0
nyel-101 <dandevjr@gmail.com>
2026-06-22 08:26:39 +0000

    test(auth): implement auth tokens

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2880ef116dcc4e45a2289cb3145fbe5b = () => {
-  return '2026-05-14T08:10:38Z';
+const node_0b6dfb4f87ec4997b04e2c5cf37e4a02 = () => {
+  return '2026-06-22T08:26:39Z';
 };
